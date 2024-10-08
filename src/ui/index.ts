2c583c756d29130259b69013b88df4ad3430abd5
smartacephale <atm.mormon@protonmail.com>
2024-10-08 01:34:37 +0000

    Update index.ts

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -9,7 +9,7 @@ export class JabroniOutfitUI {
   private getRoot(rootSelector: string | undefined) {
     let root: string | undefined = rootSelector;
     if (!root) {
-      const rootEl = parseDom('<div id="tapermonkey-app" style="position: relative; z-index: 999999;"></div>');
+      const rootEl = parseDom('<div id="jabroni-outfit" style="position: relative; z-index: 999999;"></div>');
       document.body.appendChild(rootEl);
       root = "#tapermonkey-app";
     }
